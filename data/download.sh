3c1d0d1ea236a75c81a67daa2d290995f61933da
Heinrich Schuchardt <xypron.glpk@gmx.de>
2019-11-15 16:07:15 +0100

    download.sh
    Provide script to download the raw data.
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

---
+++ data/download.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# Download and extract the raw data of the DCase 2019 acoustic scene
+# classification challenge subtask A development dataset
+#
+# Cf. http://dcase.community/challenge2019/task-acoustic-scene-classification
+
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.1.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.2.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.3.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.4.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.5.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.6.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.7.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.8.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.9.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.10.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.11.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.12.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.13.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.14.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.15.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.16.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.17.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.18.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.19.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.20.zip
+wget https://zenodo.org/record/2589280/files/TAU-urban-acoustic-scenes-2019-development.audio.21.zip
+
+unzip TAU-urban-acoustic-scenes-2019-development.audio.1.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.2.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.3.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.4.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.5.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.6.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.7.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.8.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.9.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.10.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.11.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.12.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.13.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.14.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.15.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.16.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.17.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.18.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.19.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.20.zip
+unzip TAU-urban-acoustic-scenes-2019-development.audio.21.zip
